43203c163d5894b06e51bf3decb2c86acfe33ec6
Charles Huurman <ch@utopian.ink>
2023-11-06 14:13:07 -0600

    “fix-models-for-Insomnia”

--- Develop/server.js
+++ Develop/server.js
@@ -18,3 +18,7 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
 
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+
